559122230080c41c209cdfe47e1e6123e59d1ef6
guymarsden01@gmail.com <guymarsden01@gmail.com>
2018-05-27 08:40:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,20 +11,7 @@ const codes = [
   "a"
 ];
 
-function checkCode(codeIn) {
-  if (codeIn == '38384040373937396566') {
-		alert('Congratulations! That is the Konami code!') 
-    }
-	else {
-		alert('Nope, thats not the Konami code!') }
-	return ''
-}
-
-function setDOM() {
-  document.body.addEventListener('keydown',function(e) {
-    entCode+=e.which
-  })
-}
+function key
 
 function init() {
   // your code here
